0fff36aa22262dd18f7fd647b5ce811df145f811
MyDataKitchen <alfred_loh@Alfreds-MacBook-Pro.local>
2022-03-22 21:19:02 +0800

    New python file.

---
+++ Week-1/Assignment-4/binary_search_position.py
@@ -0,0 +1,21 @@
+def binary_search_position(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+
+    while left <= right:
+      mid = int((left + right) / 2)
+
+      if numbers[mid] < target:
+        left = mid + 1
+
+      elif numbers[mid] > target:
+        right = mid - 1
+
+      else:
+        return mid
+
+    return -1
+
+
+print(binary_search_position([1, 2, 5, 6, 7], 1)) # should print 0 
+print(binary_search_position([1, 2, 5, 6, 7], 6)) # should print 3
