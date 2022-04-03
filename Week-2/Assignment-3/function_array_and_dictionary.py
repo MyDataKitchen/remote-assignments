57764d58a518b82643014b45dfdf53971f1db6da
MyDataKitchen <alfred_loh@Alfreds-MacBook-Pro.local>
2022-04-03 20:24:59 +0800

    assignment 3 updated.

--- Week-2/Assignment-3/function_array_and_dictionary.py
+++ Week-2/Assignment-3/function_array_and_dictionary.py
@@ -3,7 +3,7 @@ def avg(data):
   for product in data['products']:
     sum += product['price']
   
-  return sum
+  return round(sum / len(data["products"]), 3)
 
 
 print( avg({"products": [ {"name": "Product 1","price": 100 },{"name": "Product 2", "price": 700}, {"name": "Product 3","price": 300 }] })) # print the average price of all products (round to 3 decimal)
