fd0cd8fc486bb418d85bdbdb489e46ca18bb4656
MyDataKitchen <alfred_loh@Alfreds-MacBook-Pro.local>
2022-04-17 21:46:18 +0800

    new file updated.

--- Week-4/Assignment-3/app.py
+++ Week-4/Assignment-3/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS, cross_origin
 from setting import DB_CONFIG, SESSION_SECRET_KEY
 import pymysql
 import jwt
+import re
 
 def insert(params):
     db = pymysql.connect(host=DB_CONFIG['host'],
@@ -51,15 +52,21 @@ def check(params):
         return True
     else:
         return False
-    
+
+def check_email_format(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if (re.fullmatch(regex, email)):
+        return True
+    else:
+        return False
 
 app = Flask(__name__)
-cors = CORS(app)
+CORS(app)
 #app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.config['SECRET_KEY'] = SESSION_SECRET_KEY['SECRET_KEY']
 #app.config['SESSION_USE_SIGNER'] = True
-app.config['PERMANENT_SESSION_LIFETIME'] = 120
+app.config['PERMANENT_SESSION_LIFETIME'] = 60
 
 @app.route('/')
 def home():
@@ -68,7 +75,7 @@ def home():
         return render_template('index.html')
         #return jsonify({'message' : 'You are already logged in', 'user_id' : user_id})
     else:
-        return render_template('login.html')
+        return render_template('form.html')
         #return jsonify({'message' : 'Unauthorized'})
 
 @app.route('/login', methods =['POST'])
@@ -76,27 +83,34 @@ def login():
     login_info = json.loads(request.data)
     email = login_info['email']
     password = login_info['password']
+
+    if check_email_format(email):
+
+        if check(email):
+            try:
+                user_info = get(email)
+                if check_password_hash(user_info['password'], password):
+                    session['username'] = user_info['id']
+                    #return "Hello"
+                    return render_template('index.html')
+                    #return jsonify({'message' : 'You are logged in successfully'})
+                else:
+                    return "The password is incorrect, Please try again."
+                    #return jsonify({'message' : 'The password is incorrect, Please try again'})
+            except:
+                return Response(status=401)
+        else:
+            return "Couldn't find your account"
+            #return jsonify({'message' : "Couldn't find your account"}), 401
     
-    if check(email):
-        try:
-            user_info = get(email)
-            if check_password_hash(user_info['password'], password):
-                session['username'] = user_info['id']
-                return render_template('index.html')
-                #return jsonify({'message' : 'You are logged in successfully'})
-            else:
-                return jsonify({'message' : 'The password is incorrect, Please try again'})
-        except:
-            return Response(status=401)
     else:
-        return jsonify({'message' : "Couldn't find your account"}), 401
-    
+        return "Invalid Email"
 
 @app.route('/logout',  methods=['GET'])
 def logout():
     if 'username' in session:
         session.pop('username', None)
-    return render_template('login.html')
+    return render_template('form.html')
     #return jsonify({'message' : 'You successfully logged out'})
 
 @app.route('/register', methods=['POST'])
@@ -106,26 +120,29 @@ def register():
     password = user_info['password']
     comfirm_password = user_info['confirm_password']
 
-    if check(email):
-        return jsonify({'message' : 'The email is taken. Please try another.'}), 409 # The email is taken.
-    
-    else:
-        if password == comfirm_password:
-            password_hash = generate_password_hash(password)
-
-            if insert((email, password_hash)):
-                return render_template('login.html')
-                #return jsonify({'message' : 'Registration Successful'}), 201 # Registration Successful
-            else:
-                return jsonify({'message' : 'Registration Failed'}), 409 # Registration Failed
-
+    if check_email_format(email):
+        if check(email):
+            return "The email is taken. Please try another."
+            #return jsonify({'message' : 'The email is taken. Please try another.'}), 409 # The email is taken.
+        
         else:
-            return jsonify({'message' : 'Your new and confirm password are different. Please enter your passwords again.'}), 409 # Registration Failed
+            if password == comfirm_password:
+                password_hash = generate_password_hash(password)
 
-@app.route('/register_form')
-def redirect():
-    return render_template('register.html')
+                if insert((email, password_hash)):
+                    return render_template('form.html')
+                    #return jsonify({'message' : 'Registration Successful'}), 201 # Registration Successful
+                else:
+                    return "Registration Failed"
+                    #return jsonify({'message' : 'Registration Failed'}), 409 # Registration Failed
+
+            else:
+                return "Your new and confirm password are different. Please enter your passwords again."
+                #return jsonify({'message' : 'Your new and confirm password are different. Please enter your passwords again.'}), 409 # Registration Failed
+
+    else:
+        return "Invalid Email"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000, host='0.0.0.0')
+    app.run(debug=True, port=3000, host='192.168.0.180')
